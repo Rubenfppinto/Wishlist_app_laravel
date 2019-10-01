6fc5e1ee9f68e443cbb22e50d671249487121129
Rubenfppinto <rpinto@spartaglobal.com>
2019-10-01 21:32:42 +0100

    generated authetication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My Wishlist
                 </div>
 
                 <div class="links">
